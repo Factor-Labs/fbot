2be076bf32d638e1b3bfe7c35388636ac591918a
Arunesh Mishra <aruneshm@gmail.com>
2019-09-12 11:01:46 -0700

    green background button for popup

--- one/background.js
+++ one/background.js
@@ -2,6 +2,9 @@
 
 /* Guide at: https://developer.chrome.com/extensions/background_pages */
 
+ chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log('The color is green.');
+    });
 
 
 
